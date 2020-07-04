6117e0857fb945030162db3d9c4c8c9e6a524be4
kyle <kylecalbert@gmail.com>
2020-07-04 15:40:57 +0100

    Added comments on how to download secrets_json file

--- secrets.py
+++ secrets.py
@@ -2,5 +2,7 @@
 spotify_token = ""
 spotify_user_id = ""
 
+# Will need to download your youtube secrets file, it will be downloaded in json format, name it client_secret.json
+# place the json file in the project folder
 
-
+#https://www.youtube.com/watch?v=HzICUriU3k0 follow this youtube tutorial on how to download your youtube JSON FILE
