d30a29d2a3296f7819a0ce1db744f31456598725
kyle <kylecalbert@gmail.com>
2020-07-04 15:34:23 +0100

    Few changes made

--- secrets.py
+++ secrets.py
@@ -1,6 +1,6 @@
 # Make sure to fill in your spotify client_secret information
-spotify_token = "BQDGGIUTHPzbFzwh0fj06dlOTL4Xrnt19HJRr5uKJ_4HjCj-1b3yNN-fEE4XWv92pmiIOJP3l9TjImHlqQN92wkj4W6hadFtji63EtU3OaBSAFj-EZQKEtbXjlajxWug1h9zlEuxlhbkNiubzVgFaao7Kxwb3er9BXKLQ4PRdZQv09QjS88d8C0xyaZBSIuOXVBoLVhyTZFI4va25Vc075dkqMs9jXm7p1ZGwKKmMfgP5Xwaqr8-CfsUexpvzYzvFYdrzy__tD-r81yDNVafFGVFQHHJvKvj"
-spotify_user_id = "tnip6kiwc75fa967fi5cgenwu"
+spotify_token = ""
+spotify_user_id = ""
 
 
 
